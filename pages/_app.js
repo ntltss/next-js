b05399af593a98930803f32051e225d87f899f09
HiroyukiWindows <nevertoolatetostartsomething@gmail.com>
2022-02-27 12:12:53 +0900

    getServerSidePropsで実装完了時点

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  // return (
+  //   <Layout>
+  //       <Component {...pageProps} />
+  //   </Layout>
+  // );
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp

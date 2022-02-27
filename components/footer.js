464aefe2ff188b1cf768ba70b25b623c7b19a76f
HiroyukiWindows <nevertoolatetostartsomething@gmail.com>
2022-02-27 19:39:07 +0900

    tailwindCSS導入＆いろいろ実験完了直後

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
-export default function Footer(){
-    const [count, setCount] = useState(0);
+export default function Footer() {
+  const [count, setCount] = useState(0);
 
-    return(
-        <div>
-            <p>{count}</p>
-            <div>
-                <button onClick={()=>setCount(count+1)}>Count+</button>
-            </div>
-            <p>Footerコンポーネント</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>{count}</p>
+      <div>
+        <button
+          className="bg-indigo-700 font-semibold text-white py-2 px-4"
+          onClick={() => setCount(count + 1)}
+        >
+          Count+
+        </button>
+      </div>
+      <p>Footerコンポーネント</p>
+    </div>
+  );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,46 @@
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 export default function About() {
-    return <h1>About Page</h1>;
+  function handleClick(e) {
+    e.preventDefault();
+    console.log(`You clicked submit.`);
   }
 
-About.getLayout = function getLayout(page){
-    return <Layout>{page}</Layout>
+  function handleMouseOver(e) {
+    e.preventDefault();
+    console.log(`You MouseOver submit.`);
+  }
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <img src="/favicon.ico" alt="ファビコン" />
+      <img src="image/fujisan.jpg" alt="富士山" width="500" height="200" />
+      <input
+        type="submit"
+        value="確定"
+        className="text-2xl text-left bg-yellow-100 my-5 border-solid border-4 block w-32 rounded-md border-red-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+      />
+      <div className="flex justify-around">
+        <div>test1</div>
+        <div>test2</div>
+      </div>
+      <form
+        onClick={handleClick}
+        className="w-32 bg-gray-500 font-semibold text-white py-2 px-4"
+      >
+        <button type="submit">onClick</button>
+      </form>
+      <form
+        onMouseOver={handleMouseOver}
+        className="w-32 bg-green-500 font-semibold text-white py-2 px-4"
+      >
+        <button type="submit">onMouseOver</button>
+      </form>
+    </div>
+  );
+}
+
+About.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
 };
